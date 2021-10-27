00b494a890a185737f79fe5b876f749a2a0a8c83
lym <gliuyiming@gmail.com>
2021-10-27 21:19:40 +0800

    update

---
+++ myspace-app-im/src/main/java/me/lym/myspace/app/im/AppIMApplication.java
@@ -0,0 +1,12 @@
+package me.lym.myspace.app.im;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppIMApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppIMApplication.class,args);
+    }
+}
